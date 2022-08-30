20a2c1b7198701d95a135d8cdadbe9cbe9153ee7
basicallygit <91993321+basicallygit@users.noreply.github.com>
2022-08-30 22:44:34 +0000

    Revert changes to add termux support

--- install.sh
+++ install.sh
@@ -6,38 +6,21 @@ fi
 cargo build --release
 echo "add cee binary to /usr/bin? (y/N)"
 read choice
-#check if on termux - path for usr/bin is different
-SUDONEEDED=1
-if [[ "$PREFIX" == *com.termux* ]]
-then
-    BINPATH="${$PREFIX}/bin/cee"
-    SUDONEEDED=0
-else
-    BINPATH="/usr/bin/cee"
-fi
 case $choice in 
     [yY])
-        if [ -e $BINPATH ]
+        if [ -e /usr/bin/cee ]
         then
             echo "/usr/bin/cee exists, overwrite? (y/N)"
             read overwrite
             case $overwrite in 
                 [yY])
-                    if [ $SUDONEEDED -eq 1 ]; then
-                        sudo cp ./target/release/cee $BINPATH
-                    else
-                        cp ./target/release/cee $BINPATH
-                    fi
+                    sudo cp ./target/release/cee /usr/bin/cee
                     exit;;
                 *)
                     exit;;
             esac
         else
-            if [ $SUDONEEDED -eq 1 ]; then
-                sudo cp ./target/release/cee $BINPATH
-            else
-                cp ./target/release/cee $BINPATH
-            fi
+            sudo cp ./target/release/cee /usr/bin/cee
         fi
         exit;;
     *)
